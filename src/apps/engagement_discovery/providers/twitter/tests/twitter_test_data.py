b7cec22efb761187139425c6b86d6544b9562e19
Scott Coates <scoarescoare@gmail.com>
2014-07-27 20:37:02 -0700

    added fb tests

--- src/apps/engagement_discovery/providers/twitter/tests/twitter_test_data.py
+++ src/apps/engagement_discovery/providers/twitter/tests/twitter_test_data.py
@@ -1,64 +1,63 @@
 import datetime
 import pytz
+import factory
 
 
 eastern_time_zone = pytz.timezone('US/Eastern')
 
-# region tweet1
-tweet_1_base_tweet_1 = {
-  "in_reply_to_user_id": None,
-  "retweet_count": 0,
-  "favorite_count": 0,
-  "user": {
-    "statuses_count": 100,
-    "followers_count": 1000,
-    "friends_count": 1000,
-  },
-}
 
-tweet_1_reply_tweet_1 = dict(
-  tweet_1_base_tweet_1,
+# region tweet1
+class TwitterUserFactory(factory.Factory):
+  FACTORY_FOR = dict
+
+  statuses_count = 100
+  followers_count = 1000
+  friends_count = 1000
+
+
+class TweetFactory(factory.Factory):
+  FACTORY_FOR = dict
+
+  in_reply_to_user_id = None
+  retweet_count = 0
+  favorite_count = 0
+
+  user = factory.SubFactory(TwitterUserFactory)
+
+
+tweet_1_reply_tweet_1 = TweetFactory.build(
   in_reply_to_user_id=123
 )
 
-tweet_1_reply_tweet_2 = dict(
-  tweet_1_base_tweet_1,
+tweet_1_reply_tweet_2 = TweetFactory.build(
   in_reply_to_user_id=None
 )
 
-tweet_1_favorite_tweet_1 = dict(
-  tweet_1_base_tweet_1,
+tweet_1_favorite_tweet_1 = TweetFactory.build(
   favorite_count=123
 )
 
-tweet_1_favorite_tweet_2 = dict(
-  tweet_1_base_tweet_1,
+tweet_1_favorite_tweet_2 = TweetFactory.build(
   favorite_count=1
 )
 
-tweet_1_user_status_1 = dict(
-  tweet_1_base_tweet_1,
-  user=dict(tweet_1_base_tweet_1["user"], statuses_count=10)
+tweet_1_user_status_1 = TweetFactory.build(
+  user__statuses_count=10
 )
 
-tweet_1_user_status_2 = dict(
-  tweet_1_base_tweet_1,
-  user=dict(tweet_1_base_tweet_1["user"], statuses_count=100)
+tweet_1_user_status_2 = TweetFactory.build(
+  user__statuses_count=100
 )
 
-tweet_1_user_following_ratio_1 = dict(
-  tweet_1_base_tweet_1,
-  user=dict(
-    tweet_1_base_tweet_1["user"],
+tweet_1_user_following_ratio_1 = TweetFactory.build(
+  user=TwitterUserFactory.build(
     followers_count=1000,
     friends_count=100,
   )
 )
 
-tweet_1_user_following_ratio_2 = dict(
-  tweet_1_base_tweet_1,
-  user=dict(
-    tweet_1_base_tweet_1["user"],
+tweet_1_user_following_ratio_2 = TweetFactory.build(
+  user=TwitterUserFactory.build(
     followers_count=1000,
     friends_count=1000,
   )
