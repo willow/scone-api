4fa5e9e7c18c305841c9c59fc5b61051d13d9d7b
Scott Coates <scoarescoare@gmail.com>
2014-07-30 08:53:57 -0700

    added test data

--- src/aggregates/engagement_assignment/tests/ea_test_data.py
+++ src/aggregates/engagement_assignment/tests/ea_test_data.py
@@ -1,19 +1,61 @@
+from django.utils import timezone
 from faker import Factory
 import factory
 from factory import fuzzy
+from functools import partial
 from src.aggregates.client.enums import ClientTypeChoices
 from src.aggregates.client.models import Client
+from src.aggregates.engagement_opportunity.models import EngagementOpportunity
+from src.aggregates.profile.models import Profile
+from src.aggregates.prospect.models import Prospect
+from src.apps.engagement_discovery.enums import ProviderChoices, ProviderActionChoices
 import uuid
 
 
 faker = Factory.create()
 
+str_uid = partial(str, uuid.uuid1)
+
+
 class ClientFactory(factory.DjangoModelFactory):
   class Meta:
     model = Client
 
-  client_uid = factory.fuzzy.FuzzyAttribute(uuid.uuid1)
+  client_uid = factory.fuzzy.FuzzyAttribute(str_uid)
   client_name = factory.fuzzy.FuzzyAttribute(faker.name)
   client_type = factory.fuzzy.FuzzyChoice(ClientTypeChoices)
   enabled = True
 
+
+class ProspectFactory(factory.DjangoModelFactory):
+  class Meta:
+    model = Prospect
+
+  prospect_uid = factory.fuzzy.FuzzyAttribute(str_uid)
+  prospect_attrs = {}
+
+
+class ProfileFactory(factory.DjangoModelFactory):
+  class Meta:
+    model = Profile
+
+  profile_uid = factory.fuzzy.FuzzyAttribute(str_uid)
+  profile_external_id = factory.fuzzy.FuzzyAttribute(str_uid)
+  provider_type = factory.fuzzy.FuzzyChoice(ProviderChoices)
+  profile_attrs = {}
+  prospect = factory.SubFactory(ProspectFactory)
+  system_created_date = factory.fuzzy.FuzzyAttribute(timezone.now)
+
+
+class EngagementOpportunityFactory(factory.DjangoModelFactory):
+  class Meta:
+    model = EngagementOpportunity
+
+  engagement_opportunity_uid = factory.fuzzy.FuzzyAttribute(str_uid)
+  engagement_opportunity_external_id = factory.fuzzy.FuzzyAttribute(str_uid)
+  engagement_opportunity_attrs = {}
+  profile = factory.SubFactory(ProfileFactory, provider_type=factory.SelfAttribute('..provider_type'))
+  provider_type = factory.fuzzy.FuzzyChoice(ProviderChoices)
+  provider_action_type = factory.fuzzy.FuzzyChoice(ProviderActionChoices)
+  created_date = factory.fuzzy.FuzzyAttribute(timezone.now)
+  system_created_date = factory.fuzzy.FuzzyAttribute(timezone.now)
--- src/aggregates/engagement_assignment/tests/integration/test_assignment_creation.py
+++ src/aggregates/engagement_assignment/tests/integration/test_assignment_creation.py
@@ -1,10 +1,14 @@
 import pytest
 from src.aggregates.client.enums import ClientTypeEnum
-from src.aggregates.engagement_assignment.tests.ea_test_data import ClientFactory
+from src.aggregates.engagement_assignment.tests.ea_test_data import ClientFactory, EngagementOpportunityFactory
+from src.apps.engagement_discovery.enums import ProviderEnum, ProviderActionEnum
 
 
 @pytest.mark.django_db()
 def test_saas_rules_engine_score():
-  assert ClientFactory.create(
-    client_type=ClientTypeEnum.saas_tech_startup
+  client = ClientFactory.create(client_type=ClientTypeEnum.saas_tech_startup)
+
+  eo = EngagementOpportunityFactory.create(
+    provider_type=ProviderEnum.twitter,
+    provider_action_type=ProviderActionEnum.twitter_tweet
   )
