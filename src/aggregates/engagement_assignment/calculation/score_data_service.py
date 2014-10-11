c0d05e5637ec265ec97180d54683b6c98b8ba8a8
Scott Coates <scoarescoare@gmail.com>
2014-10-10 20:28:00 -0700

    saving data

--- src/aggregates/engagement_assignment/calculation/score_data_service.py
+++ src/aggregates/engagement_assignment/calculation/score_data_service.py
@@ -13,23 +13,21 @@ def get_client_score_data(client):
   return _get_client_score_bounds(client)
 
 
-def _get_highest_score(scores_list, _array_utils=None):
-  if not _array_utils: _array_utils = array_utils
-
+def _get_highest_score(scores_list, _array_utils):
   scores_array = np.array(scores_list)
   highest_prospect_score = float(_array_utils.filter_by_median_absolute_deviation(scores_array).max())
   return highest_prospect_score
 
 
-def set_client_bounds_score(client, _score_data_repository=None):
+def set_client_bounds_score(client, _score_data_repository=None, _array_utils=None):
   if not _score_data_repository: _score_data_repository = score_data_repository
+  if not _array_utils: _array_utils = array_utils
 
   ret_val = {}
   recent_scores = score_data_repository.get_recent_client_scores(client)
 
   prospect_scores = recent_scores.pop(constants.PROSPECT_RECENT_SCORES)
-  highest_prospect_score = _get_highest_score(prospect_scores)
+  highest_prospect_score = _get_highest_score(prospect_scores, _array_utils)
   ret_val[constants.PROSPECT_UPPER_BOUND_SCORE] = highest_prospect_score
-
 
   _score_data_repository.save_client_prospect_bounds_score(client, highest_prospect_score)
--- src/aggregates/engagement_assignment/tests/unit/test_score_data_service.py
+++ src/aggregates/engagement_assignment/tests/unit/test_score_data_service.py
@@ -1,10 +1,12 @@
 import pytest
+import numpy as np
 from unittest.mock import MagicMock
 
 from src.aggregates.engagement_assignment import constants
 from src.aggregates.engagement_assignment.calculation import score_data_service, score_data_repository
 from src.aggregates.engagement_assignment.tests.ea_test_data import client_1, \
   client_1_scores
+from src.libs.numpy_utils import array_utils
 
 
 def _get_score_data_repo():
@@ -14,10 +16,17 @@ def _get_score_data_repo():
   return score_data_repo_mock
 
 
+def _get_array_utils():
+  array_utils_mock = MagicMock(spec=array_utils)
+  array_utils_mock.filter_by_median_absolute_deviation.return_value = np.array([10.])
+  return array_utils_mock
+
+
 # region test bounds
 def test_score_data_service_assigns_correct_prospect_upper_bound_score():
   score_data_repo_mock = _get_score_data_repo()
-  _, ret_val = score_data_service.set_client_bounds_score(client_1, score_data_repo_mock)
+  array_utils_mock = _get_array_utils()
+  _, ret_val = score_data_service.set_client_bounds_score(client_1, score_data_repo_mock, array_utils_mock)
   assert round(ret_val[constants.PROSPECT][constants.SCORE], 3) == 0.214
 
 # endregion test bounds
